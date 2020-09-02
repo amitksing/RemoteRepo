12ad344db3392bf7793acfe5078b2847dff4cf84
Singh <amir.singh@capgemini.com>
2020-09-02 23:20:32 +0530

    my java project

---
+++ src/test/java/com/cg/project/test/MainTest.java
@@ -0,0 +1,28 @@
+package com.cg.project.test;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MainTest {
+
+	@Test
+	
+	public void test1() {
+		
+		Assert.fail("Test 1 failed");
+	}
+	
+@Test
+	
+	public void test2() {
+		
+	}
+
+@Test
+
+public void test3() {
+	
+	Assert.fail("Test 3 failed");
+}
+
+}
